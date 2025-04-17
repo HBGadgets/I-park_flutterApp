4d786548f09ea5f777bb8429839d2498cfe5f05e
Harsh <harshalnehare7@gmail.com>
2025-04-17 15:52:19 +0530

    working on profile page

--- lib/view/ui/home.dart
+++ lib/view/ui/home.dart
@@ -11,11 +11,7 @@ class HomePage extends StatefulWidget {
 class HomeScreen extends State<HomePage> {
   int selectedTabIndex = 0;
 
-  final List<String> appBarTitles = [
-    'Home',
-    'History',
-    'Profile',
-  ];
+  final List<String> appBarTitles = ['Home', 'History', 'Profile'];
 
   void onTabChanged(int index) {
     setState(() {
@@ -61,9 +57,18 @@ class HomeScreen extends State<HomePage> {
                 unselectedLabelColor: Colors.white.withAlpha(153),
                 onTap: onTabChanged,
                 tabs: const [
-                  Tab(text: 'Home', icon: Icon(Icons.home, size: 30, color: Colors.white)),
-                  Tab(text: 'History', icon: Icon(Icons.history, size: 30, color: Colors.white)),
-                  Tab(text: 'Profile', icon: Icon(Icons.person, size: 30, color: Colors.white)),
+                  Tab(
+                    text: 'Home',
+                    icon: Icon(Icons.home, size: 30, color: Colors.white),
+                  ),
+                  Tab(
+                    text: 'History',
+                    icon: Icon(Icons.history, size: 30, color: Colors.white),
+                  ),
+                  Tab(
+                    text: 'Profile',
+                    icon: Icon(Icons.person, size: 30, color: Colors.white),
+                  ),
                 ],
               ),
             ),
@@ -102,15 +107,14 @@ class HomeScreen extends State<HomePage> {
                       const SizedBox(width: 10),
                       const Text(
                         'Rohan Rah',
-                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ],
                   ),
-                  Image.asset(
-                    'assets/images/qr.png',
-                    width: 259,
-                    height: 259,
-                  ),
+                  Image.asset('assets/images/qr.png', width: 259, height: 259),
                   const Text(
                     'Scan QR',
                     style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
@@ -126,23 +130,11 @@ class HomeScreen extends State<HomePage> {
             child: ListView(
               children: [
                 ChatItem('MH36A6678', 'I Need my car in 30 min'),
-                const Divider(
-                  color: Colors.black,
-                  thickness: 1,
-                  height: 15,
-                ),
+                const Divider(color: Colors.black, thickness: 1, height: 15),
                 ChatItem('MH36A6678', 'I Need my car in 5 min'),
-                const Divider(
-                  color: Colors.black,
-                  thickness: 1,
-                  height: 15,
-                ),
+                const Divider(color: Colors.black, thickness: 1, height: 15),
                 ChatItem('MH36A6678', 'I Need my car in 30 min'),
-                const Divider(
-                  color: Colors.black,
-                  thickness: 1,
-                  height: 15,
-                ),
+                const Divider(color: Colors.black, thickness: 1, height: 15),
               ],
             ),
           ),
@@ -152,14 +144,120 @@ class HomeScreen extends State<HomePage> {
   }
 
   Widget buildHistoryTab() {
-    return Center(
-      child: Text('History Tab Content'),
-    );
+    return Center(child: Text('History Tab Content'));
   }
 
   Widget buildProfileTab() {
     return Center(
-      child: Text('Profile Tab Content'),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            backgroundColor: Colors.grey[300],
+            radius: 90,
+            backgroundImage: AssetImage(
+              'assets/images/Photo by Petr Sevcovic.png',
+            ),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Rohan Rah',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 8),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(
+              5,
+              (index) => Icon(Icons.star, color: Colors.amber, size: 15),
+            ),
+          ),
+          const SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Employee ID',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text('EMP0001111',style: TextStyle(color: Colors.grey,fontSize: 16),),
+                ],
+
+              ),
+              Column(
+                children: [
+                  Text(
+                    'Contact no',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text('+91 75484856',style: TextStyle(color: Colors.grey,fontSize: 16)),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: 8),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Job Title',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                'Supervisor',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 16),
+          Container(
+            margin: const EdgeInsets.all(12),
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(19),
+              border: Border.all(color: Colors.black, width: 1),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Feedback:',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  '"I recently had the pleasure of experiencing Valley Boy, and I have to say, it was an absolute delight! The music is fresh, catchy, and the lyrics are both thoughtful and relatable. Valley Boy has a unique sound that combines laid-back vibes with an edgy twist, creating a perfect balance for a variety of moods."',
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(fontSize: 14, color: Colors.grey[600]),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
