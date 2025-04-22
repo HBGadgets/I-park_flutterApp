3aa95be7e6a560d1c9b007cd96344ec9faeb6ddd
aleenaazam <azamaleena9823@gmail.com>
2025-04-22 10:40:44 +0530

    added permission

--- lib/controller/user_login_api.dart
+++ lib/controller/user_login_api.dart
@@ -1,22 +1,28 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import '../view/constants/constant_url.dart';
 
 Future<bool> login(String email, String password) async {
-  final String apiUrl = ConstantUrl.loginUrl;
+  final String apiUrl = 'https://i-park.onrender.com/api/user/login';
 
   try {
     final response = await http.post(
       Uri.parse(apiUrl),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'email': email, 'password': password}),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        'email': email,
+        'password': password,
+      }),
     );
 
     if (response.statusCode == 200) {
+
       final data = jsonDecode(response.body);
       print('Login successful: ${data['token']}');
       return true;
     } else {
+
       print('Login failed: ${response.body}');
       return false;
     }
