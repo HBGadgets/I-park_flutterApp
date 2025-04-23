21b763351bedf6429d6e7fa4061146fa3ff43f33
Harsh <harshalnehare7@gmail.com>
2025-04-23 12:40:57 +0530

    update constants

--- lib/view/constants/constant_colors.dart
+++ lib/view/constants/constant_colors.dart
@@ -4,7 +4,7 @@ class ConstantColors {
   static const Color primaryColor = Colors.black;
   static Color registerColor = Colors.blue.shade900;
   static const Color iconColor = Colors.black;
-  static const Color textFieldText = Colors.black;
+  static const Color textFieldText = Color(0xFF000842);
   static const Color rememberColor = Colors.grey;
   static const Color forgetColor = Colors.grey;
   static const Color loginBButtonColor = Colors.black;
